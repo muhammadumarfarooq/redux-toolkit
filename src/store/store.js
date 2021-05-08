51b8c9242dd90547ec9e260ebb6f7f9d9fd7acc0
Muhammad Umar Farooq <develop.umar@gmail.com>
2021-05-08 20:38:15 +0500

    configure store using redux toolkit

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+
+//This will create a redux store and automatically configure the redux devTools extension.
+//
+export default configureStore({
+  reducer: {},
+});
